180b2b668c725b2c13f0863417b9b4ac1e6e5eb6
MrUniver <mr1strange@gmail.com>
2023-01-06 17:53:44 +0100

    modif css

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
   <!-- Partie avec graphique -->
   <div id="graph">
-    <div class="graph-close-open" @click="open_or_close">
+    <div class="graph__close-open">
       <svg id="more-arrows">
         <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 " />
         <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 " />
